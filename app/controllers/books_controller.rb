f9e38cd6be5b76bc8c6dc6aa9e8d3a8fbe5b31b5
qirpi <qirpi@lambd.ax>
2023-01-03 23:05:41 +0100

    basic ability to upload ebooks, no full-text extraction yet

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,4 +10,19 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'Book added'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(%i[title author epub cover])
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
   has_many :annotations
+  has_one_attached :epub
 end
