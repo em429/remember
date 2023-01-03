0583765ba9a0712296785dbfe571ed2524486f87
qirpi <qirpi@lambd.ax>
2023-01-03 23:15:38 +0100

    adds covers and placeholder helper

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,9 @@
 module BooksHelper
+  def book_cover(book)
+    if book.cover.attached?
+      image_tag(book.cover)
+    else
+      image_tag('book-cover-placeholder.jpg')
+    end
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   has_many :annotations
   has_one_attached :epub
+  has_one_attached :cover
 end
