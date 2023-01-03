73c5b9d2fd6f5439b909b8a8837f343cef80c022
qirpi <qirpi@lambd.ax>
2023-01-03 23:37:37 +0100

    adds delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,6 +23,13 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, alert: 'Book successfully deleted'
+  end
+
   private
 
   def book_params
