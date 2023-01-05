5faab50a01f4605a5b76e7d918dee378ab023dff
qirpi <qirpi@lambd.ax>
2023-01-05 23:02:46 +0100

    add full_text_extract form helper and move annotation import to book#show page

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,16 +1,25 @@
 module BooksHelper
   def fiction_or_not_tag(book)
     if book.fiction
-      "Fiction"
+      'Fiction'
     else
-      "Non-Fiction"
+      'Non-Fiction'
     end
   end
+
   def book_cover(book)
     if book.cover.attached?
       image_tag(book.cover)
     else
       image_tag('book-cover-placeholder.jpg')
+    end
+  end
+
+  def full_text_extract_form(book)
+    if !book.full_text.blank?
+      link_to 'Extracted full text is available in the database for this book.', book_full_text_path(book)
+    else
+      button_to 'Extract Text', book_start_text_extraction_path(book), method: :post, class: "btn"
     end
   end
 end
