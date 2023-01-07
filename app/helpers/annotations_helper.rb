487e5cefb40c040333f176eba1dd8b5fa5b01528
qirpi <qirpi@lambd.ax>
2023-01-07 20:08:52 +0100

    add comments to explain current state of affairs

--- app/helpers/annotations_helper.rb
+++ app/helpers/annotations_helper.rb
@@ -9,9 +9,13 @@ module AnnotationsHelper
     # content_tag(:div, annotation.notes, class: 'p-6 shadow-md border')
   end
 
+  # Grep wouldn't find 'cross-boundary' highlights; e.g. anything with newlines (\n) in them.
+  #   - Because of this, we split by \n and only search for the first part
+  #   - It re-adds the consequential parts to the output after search matched
   def show_paragraph_of(annotation)
     full_text = annotation.book.full_text
 
+    # We split and only search for the first part, before the newline
     hl_split = annotation.highlighted_text.split("\n")
     highlighted_text = hl_split[0]
 
@@ -21,13 +25,17 @@ module AnnotationsHelper
     match = Regexp.last_match
     if match
       if hl_split.size > 1
+        # Re-add the previously stripped parts to the matched search result
         "#{match}\n#{hl_split[1..].join(' ')}"
       else
         match
       end
     else
+      # TODO: If the highlight doesn't match due to other errors, show original for now.
+      #   - e.g. nokogiri strips some special chars when converting epub to text,
+      #     but the highlights have it still..
       "Showing original: #{annotation.highlighted_text}"
     end
-    
   end
+
 end
