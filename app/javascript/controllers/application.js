10df41b18496be0331539138398be82e586af15a
qirpi <qirpi@lambd.ax>
2023-01-07 15:54:08 +0100

    add stimulus-reveal component

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,12 @@
 import { Application } from "@hotwired/stimulus"
 
+// Import extra stimulus-components, pinned using the importmap command
+import Reveal from 'stimulus-reveal-controller'
+
 const application = Application.start()
+
+// Register extra stimulus-components
+application.register('reveal', Reveal)
 
 // Configure Stimulus development experience
 application.debug = false
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,7 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.1
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "stimulus-reveal-controller" # @4.1.0
