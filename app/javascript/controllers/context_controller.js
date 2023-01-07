d1237d524d3a4624f90ad7fbabbdf8d43748f67e
qirpi <qirpi@lambd.ax>
2023-01-07 20:15:09 +0100

    show annotation context on cards: first working PoC version

---
+++ app/javascript/controllers/context_controller.js
@@ -0,0 +1,9 @@
+import Reveal from 'stimulus-reveal-controller'
+
+export default class extends Reveal {
+  toggle() {
+    // We just simply call the parent class's toggle for now
+    super.toggle()
+    // Add anythinge extra here
+  }
+}
