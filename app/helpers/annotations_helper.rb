abb84171fca36c3083a9df549647adce71424be2
qirpi <qirpi@lambd.ax>
2023-01-08 18:35:24 +0100

    show context: fix some bugs, but new implementation is needed bc this is too hacky

--- app/helpers/annotations_helper.rb
+++ app/helpers/annotations_helper.rb
@@ -10,7 +10,7 @@ module AnnotationsHelper
 
   def show_neighbors_of(annotation)
     full_text = annotation.book.full_text
-    original_hl = annotation.highlighted_text
+    original_hl = annotation.highlighted_text.strip
 
     # We split and only search for the first part, before the newline
     original_hl_split = original_hl.split("\n")
@@ -27,15 +27,17 @@ module AnnotationsHelper
     full_text =~ /^#{context}(#{regexp})#{context}/
 
     before = Regexp.last_match(1)
-    matched_paragraph = "#{Regexp.last_match(2)}#{second_half}"
+    # Here we don't readd the second half, (unlike in show_paragraph_of),
+    # as it will be contained in $after regex match, so it would produce
+    # a duplicate line.
+    matched_paragraph = Regexp.last_match(2).to_s
     after = Regexp.last_match(3)
-    if matched_paragraph
-      # Return match with neighboring lines
-      # Re-add the previously stripped parts to the matched search result
+
+    # Return match with neighboring lines
+    if !matched_paragraph.blank?
+      # We split by the original highlight's first half
       split = "#{before}#{matched_paragraph}#{after}".split(first_half)
-      # Now we split the full context string by the original highlighted text, to get the
-      # start and end points for our span tag.
-      raw("#{split[0]}<span class=\"bg-yellow-100\">#{original_hl_without_newline}</span>#{split[1]}")
+      raw("#{split[0]}<span class=\"bg-yellow-100\">#{first_half}</span>#{split[1]}")
     else
       "Couldn't load extra context."
     end
@@ -46,29 +48,34 @@ module AnnotationsHelper
   #   - We re-add the consequential parts to the output after search matched
   def show_paragraph_of(annotation)
     full_text = annotation.book.full_text
-    original_hl = annotation.highlighted_text
+    original_hl = annotation.highlighted_text.strip
 
     # We split and only search for the first part, before the newline
     original_hl_split = original_hl.split("\n")
 
-    first_half = original_hl_split[0]
-    second_half = original_hl_split[1..].join(' ')
+    first_half = original_hl_split[0].remove("\n")
+    second_half = original_hl_split[1..].join(' ').remove("\n")
     original_hl_without_newline = "#{first_half}#{second_half}"
 
     regexp = /.*#{Regexp.escape(first_half)}.*/
     full_text =~ /#{regexp}/
 
-    # FIXME: i was using 726 as one of the examples
+    # FIXME: -- very buggy, repeats itself.. etc
+    # One of the bugs are caused by newlines, (\n) in text
+    # i was using 726 as one of the examples
+
+    # Here we re-add the second half
     matched_paragraph = "#{Regexp.last_match}#{second_half}"
-    if matched_paragraph
-      split = matched_paragraph.to_s.split(first_half)
+    if !matched_paragraph.blank?
+      split = matched_paragraph.to_s.split(original_hl_without_newline)
       # Add yellow color to the original highlighted text (without newlines now :) )
       raw("#{split[0]}<span class=\"bg-yellow-100\">#{original_hl_without_newline}</span>#{split[1]}")
+
     else
-      # TODO: If the highlight doesn't match due to other errors, show original for now.
+      # FIXME: If the highlight doesn't match due to other errors, show original for now.
       #   - e.g. nokogiri strips some special chars when converting epub to text,
       #     but the highlights have it still..
-      "Showing original: #{original_hl}"
+      "Couldn't load containing paragraph, likely due to HTML entity character mismatch."
     end
   end
 end
