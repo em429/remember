6a4330ea2fda5b91e45c8613f12d615e3a77655b
qirpi <qirpi@lambd.ax>
2023-02-04 21:30:22 +0100

    header refactor + adding active indicator to current page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def nav_link_to(title, url)
+    if current_page?(url)
+      link_to(title, url, class: "active")
+    else
+      link_to(title, url)
+    end
+  end
+  
 end
