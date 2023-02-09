7ecb041a4dc61d21bf3006f510464745e6bf0c5f
qirpi <qirpi@lambd.ax>
2023-02-10 00:51:02 +0100

    add stimulus controllers pt2

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,5 @@
+import Dropdown from 'stimulus-dropdown'
+
+// Connects to data-controller="dropdown"
+export default class extends Dropdown {
+}
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,6 @@ pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.1
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-reveal-controller" # @4.1.0
+pin "stimulus-dropdown" # @2.1.0
+pin "hotkeys-js" # @3.10.1
+pin "stimulus-use" # @0.51.3
