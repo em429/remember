aa4878f71cab2c15ccbf8e605fbf23bd34c97510
qirpi <qirpi@lambd.ax>
2023-02-13 03:23:56 +0100

    add require login + skips

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
 
+  before_action :require_login
+
   def current_user
     @current_user || User.find(session[:user_id]) if session[:user_id]
   end
 
+  def require_login
+    if session[:user_id].nil?
+      redirect_to login_path, alert: "Please log in to access that page."
+    end
+  end
+
   helper_method :current_user
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
   
   def show
     @user = User.find(params[:user_id])
