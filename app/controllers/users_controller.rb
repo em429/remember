94d1f33b0b9ea6280908bb8903eedd9795340ea1
qirpi <qirpi@lambd.ax>
2023-02-13 04:09:11 +0100

    add autologin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id # automatically log-in user
       redirect_to @user, notice: "User succesfully created"
     else
       render :new, status: :unprocessable_entity
