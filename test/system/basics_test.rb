55bc02ae9ca95bdba9c1d369b061bad0d117b579
qirpi <qirpi@lambd.ax>
2023-02-24 17:40:41 +0100

    add system tests for basic user interactions

--- test/system/basics_test.rb
+++ test/system/basics_test.rb
@@ -43,34 +43,74 @@ class BasicsTest < ApplicationSystemTestCase
     assert page.has_content? "Password must be 8+ characters and must include a number" 
   end
 
-  test "adding a book" do
+  test "login add a book and import annotations" do
     visit root_url
-    click_on 'Sign up now!'
-    fill_in 'Username', with: 'test'
-    fill_in 'Email', with: 'test@test.com'
+    # Login
+    fill_in 'Email', with: 'default-test@test.com'
     fill_in 'Password', with: 'asdf1234'
-    fill_in 'Confirm password', with: 'asdf1234'
-    click_on 'Create User'
+    click_button "Log in"
     assert page.has_content? 'Profile' 
-    click_on 'Add Book'
-    assert page.has_content? 'Title'
 
-    # TODO: upload epub and cover
-    
-    
+    # Add book
+    click_link 'Add Book'
+    assert page.has_content? 'Title'
+    attach_file('book[epub]', "#{Rails.root}/test/fixtures/files/book.epub")
+    attach_file('book[cover]', "#{Rails.root}/test/fixtures/files/cover.jpg")
     fill_in 'Title', with: 'Test Book'
     fill_in 'Author', with: 'John Doe'
-    click_on 'Add Book'
-    magic_test
+    click_button 'Add Book'
+    assert page.has_content? 'Test Book'
+    assert page.has_content? 'by John Doe'
+    assert page.has_content? 'Non-Fiction'
+    assert page.has_content? 'Extracted plaintext is available in the database for this book.'
+
+    # Import annotations
+    attach_file('opf_file', "#{Rails.root}/test/fixtures/files/metadata.opf")
+    click_button 'Import'
+    assert page.has_content? 'By writing continuously, you force the edit-crazy part of your mind into a subordinate position'
+    
+    # Visit a single annotation
+    first('.simple-card > a').click
+    assert page.has_content? 'Test Book by John Doe'
+    
   end
 
-  # test "importing annotations" do
-  #   
-  # end
+  test "editing a book" do
+    visit root_url
+    # Login
+    fill_in 'Email', with: 'default-test@test.com'
+    fill_in 'Password', with: 'asdf1234'
+    click_button "Log in"
+    assert page.has_content? 'Profile'
 
-  # test "editing book" do
-  #   
-  # end
+    click_link 'My Books'
+    click_link 'Missing Text Book'
+    assert page.has_content? 'No plaintext'
+    
+    # Add missing cover and epub, change Author
+    click_link 'Edit'
+    assert page.has_content? 'Editing Missing Text Book'
+    attach_file('book[cover]', "#{Rails.root}/test/fixtures/files/cover.jpg")
+    attach_file('book[epub]', "#{Rails.root}/test/fixtures/files/book.epub")
+    fill_in "Author", with: "Mary Doh"
+    click_button 'Add Book'
+    assert page.has_content? 'Mary Doh'
+    assert page.has_content? 'Extracted plaintext is available'
+    
+  end
+
+  test "showing book with missing attachments" do
+    visit root_url
+    # Login
+    fill_in 'Email', with: 'default-test@test.com'
+    fill_in 'Password', with: 'asdf1234'
+    click_button "Log in"
+    assert page.has_content? 'Profile'
+    
+    click_link 'My Books'
+    click_link 'How to do everything'
+    assert page.has_content? 'How to do everything'
+  end
 
   # test "adding notes to annotation" do
   #   
