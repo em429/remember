65e408d44578c84c06ac464389fe516f7c37d359
qirpi <qirpi@lambd.ax>
2023-02-24 18:22:48 +0100

    add tests: books controller

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,7 +1,55 @@
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @book = books(:one)
+    @user = User.find_by(email: "default-test@test.com")
+
+    # Log-in
+    post session_url, params: { email: "default-test@test.com", password: "asdf1234" }
+    assert_redirected_to @user
+  end
+
+  test "should get index" do
+    get books_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_book_url
+    assert_response :success
+  end
+
+  test "should create book" do
+    assert_difference("Book.count") do
+      post books_url, params:
+        { book: { title: "Controller Book", author: "Joe Dogan", user_id: @user.id }}
+    end
+
+    assert_redirected_to book_url(Book.last)
+  end
+
+  test "should show book" do
+    get book_url(@book)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_book_url(@book)
+    assert_response :success
+  end
+
+  test "should update book" do
+    patch book_url(@book), params:
+      { book: { title: "Updated Book", author: "Joe Hogan", user_id: @user.id }}
+    assert_redirected_to book_url(@book)
+  end
+
+  test "should destroy book" do
+    assert_difference("Book.count", -1) do
+      delete book_url(@book)
+    end
+
+    assert_redirected_to books_url
+  end
 end
