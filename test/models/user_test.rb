3068411cdc3185e419322e57a42ca58e132eb842
qirpi <qirpi@lambd.ax>
2023-02-24 01:23:07 +0100

    add basic user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user with empty parameters" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "should not save user with whitespace as password" do
+    user = User.new(username: "test", email: "t@test.com", password: "        ")
+    assert_not user.save, "Saved user with whitespace as password"
+  end
+  
+  test "should not save user with weak password" do
+    user = User.new(username: "test", email: "t@test.com", password: "asdfasdf")
+    assert_not user.save, "Saved user with weak password"
+  end
+
+  test "should not save user with invalid email" do
+    user = User.new(username: "test", email: "test.com", password: "asdf1234")
+    assert_not user.save, "Saved invalid email"
+  end
+  
+  test "save valid user" do
+    user = User.new(username: "test", email: "t@test.com", password: "asdf1234")
+    assert user.save, "Couldn't save valid user"
+  end
+
 end
