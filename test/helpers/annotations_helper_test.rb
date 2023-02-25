243ad48af16135af494854afe82fa10ec261d516
qirpi <qirpi@lambd.ax>
2023-02-25 04:27:06 +0100

    placeholder

---
+++ test/helpers/annotations_helper_test.rb
@@ -0,0 +1,7 @@
+class AnnotationsHelperTest < ActionView::TestCase
+  # TODO: implement helper tests
+  # test "should show context" do
+  #   annotation = annotations(:one)
+  #   assert_dom_equal %{ put the highlight + context here that we expect }, show_context(annotation)
+  # end
+end
