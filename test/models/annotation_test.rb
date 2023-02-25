e0337dfeed88784bbca01e06cbad706679a46282
qirpi <qirpi@lambd.ax>
2023-02-25 04:27:01 +0100

    add model tests

--- test/models/annotation_test.rb
+++ test/models/annotation_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class AnnotationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not create blak annotation' do
+    a = Annotation.new
+    assert_not a.save, "Saved annotation with all fields blank"
+  end
+
+  test 'should create annotation' do
+    assert Annotation.create!(
+      highlighted_text: "test",
+      notes: "",
+      start_cfi: "",
+      end_cfi: "",
+      timestamp: "2022-12-14",
+      # toc_family_titles: JSON.generate(annotation_hash['toc_family_titles']),
+      toc_family_titles: "",
+      book_id: books(:one).id
+    ), "Couldn't create annotation"
+  end
+  
+  test 'should not create annotation with blank highlight' do
+    assert_not Annotation.create!(
+      highlighted_text: "",
+      notes: "",
+      start_cfi: "",
+      end_cfi: "",
+      timestamp: "2022-12-14",
+      # toc_family_titles: JSON.generate(annotation_hash['toc_family_titles']),
+      toc_family_titles: "",
+      book_id: books(:one).id
+    ), "Created annotation with blank highlight"
+  end
+
+
+  
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save book without attachments" do
+    book = Book.new(title: "Model Test", author: "John Doe", user_id: users(:mary).id)
+    assert book.save, "Couldn't save without attachemnts"
+  end
+  
+  test "should not save blank book" do
+    book = Book.new
+    assert_not book.save, "Saved blank book"
+  end
 end
