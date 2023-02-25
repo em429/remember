a16cc7eb88e42c2a75f192a2a97a4f352bd747df
qirpi <qirpi@lambd.ax>
2023-02-25 04:27:57 +0100

    reminder to fix

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,8 +1,10 @@
 module BooksHelper
+  # TODO: move to scope
   def random_highlight(book)
     book.annotations.all.order('RANDOM()').first&.highlighted_text || ''
   end
 
+  # TODO: implement as enum (fiction, non, paper, article..etc)
   def fiction_or_not_tag(book)
     if book.fiction
       'Fiction'
