ff3c9337c7ff42b270b0318f2fcdd794803e1048
qirpi <qirpi@lambd.ax>
2023-02-25 04:27:34 +0100

    tests: annotations controller

--- test/controllers/annotations_controller_test.rb
+++ test/controllers/annotations_controller_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class AnnotationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  setup do
+    @annotation = annotations(:one)
+    @user = users(:mary)
+    
+    sign_in_as(@user)
+    assert_redirected_to @user
+  end
+
+  test "should get index" do
+    get annotations_url
+    
+    assert_response :success
+  end
+
+  test "should show annotation" do
+    get annotation_url(@annotation)
+    assert_response :success
+  end
+
+  # TODO: add when annotation delete added
+  # test "should destroy annotation" do
+  #   assert_difference("Annotation.count", -1) do
+  #     delete annotation_url(@annotation)
+  #   end
+
+  #   assert_redirected_to annotations_url
   # end
 end
