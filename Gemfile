692941738754bb747c5dba5e9141af75aa94e413
qirpi <qirpi@lambd.ax>
2023-02-27 15:21:30 +0100

    add nix flake + devenv based reproducible devenv

--- Gemfile
+++ Gemfile
@@ -57,6 +57,10 @@ group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
 
+  gem "solargraph"
+  gem "rubocop"
+  gem "foreman"
+
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";    
+
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = { self, nixpkgs, devenv, ... } @ inputs:
+    let
+      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = f: builtins.listToAttrs (map (name: { inherit name; value = f name; }) systems);
+    in
+    {
+      devShells = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  languages.ruby.enable = true;
+
+                  languages.ruby.versionFile = ./.ruby-version;
+                  
+                  # https://devenv.sh/reference/options/
+                  packages = [
+                   pkgs.nodejs
+
+                   pkgs.nodePackages.vscode-html-languageserver-bin
+                   pkgs.nodePackages.vscode-css-languageserver-bin
+                   pkgs.nodePackages.typescript-language-server
+                   pkgs.nil # nix language server
+
+                   pkgs.chromedriver pkgs.ungoogled-chromium
+                    
+                  ];
+
+                  enterShell = ''
+                    bundle
+                  '';
+                }
+              ];
+            };
+          });
+    };
+}
