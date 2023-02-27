c8422b02c63484d820741506a6ec70e94b3af00d
qirpi <qirpi@lambd.ax>
2023-02-27 15:36:44 +0100

    add validation

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -5,6 +5,8 @@ class Annotation < ApplicationRecord
   scope :random_single, -> { random_all.limit(1) }
   scope :recent, -> { all.order(timestamp: :desc) }
 
+  validates :highlighted_text, presence: true, allow_blank: false
+
   def chapters
     if toc_family_titles.present?
       JSON.parse(toc_family_titles)
