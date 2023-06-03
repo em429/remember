2f44e8e4694f42b3424ed347776da4c3e770b77e
qirpi <qirpi@lambd.ax>
2023-06-03 21:19:47 +0200

    refactor

--- app/models/rss_feed.rb
+++ app/models/rss_feed.rb
@@ -4,8 +4,9 @@ require 'open-uri'
 class RssFeed < ApplicationRecord
   validates :title, :url, presence: true
 
-  def fetch_feed
-    Rails.cache.fetch("#{cache_key_with_version}/fetch_feed", expires_in: 6.hours) do
+  # Either fetches the feed, or returns the cached version
+  def cached
+    Rails.cache.fetch("#{cache_key_with_version}/cached_feed", expires_in: 6.hours) do
       URI.open(url) do |rss|
         feed = RSS::Parser.parse(rss)
       end 
