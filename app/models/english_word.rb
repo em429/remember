449f4ca6c7e35161e54157030432fe887e6b8ea6
qirpi <qirpi@lambd.ax>
2023-06-05 22:13:36 +0200

    add states to add states to english word tag

--- app/models/english_word.rb
+++ app/models/english_word.rb
@@ -1,9 +1,13 @@
 class EnglishWord < ApplicationRecord
 
-    def wordnet_definition
-      Rails.cache.fetch "#{cache_key_with_version}/wordnet_definition2", expires_in: 100.hours do
+    def fetch_wordnet_definition
+      Rails.cache.fetch "#{cache_key_with_version}/wordnet_definition", expires_in: 100.hours do
         `dict -d wn #{word}`
       end
+    end
+
+    def cached_wordnet_definition
+      Rails.cache.fetch("#{cache_key_with_version}/wordnet_definition")
     end
 
     def moby_thesaurus
