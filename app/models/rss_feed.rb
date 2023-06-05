bade2aa70c1ab401c84eaee2d2b21d2884d3da08
qirpi <qirpi@lambd.ax>
2023-06-05 22:13:39 +0200

    comment

--- app/models/rss_feed.rb
+++ app/models/rss_feed.rb
@@ -4,6 +4,9 @@ require 'open-uri'
 class RssFeed < ApplicationRecord
   validates :title, :url, presence: true
 
+  # TODO: rename to 'refresh?' fetch is confusing because of cache.fetch method which does soemthing else than this.
+  # This will always refresh the feed, no matter what, unlike a Rails.cache.fetch block, which would return the cached
+  # value if it's already present.
   def fetch
     URI.open(url) do |rss|
       feed = RSS::Parser.parse(rss)
